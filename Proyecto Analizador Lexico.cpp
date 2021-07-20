05427a083aab9bf614c9fcfd1abd4031f37f2cc1
Fausto Hernandez <39222718+Achack@users.noreply.github.com>
2021-07-19 19:41:51 -0600

    Update Proyecto Analizador Lexico.cpp
    Se han agregado los cambios de estado y la lectura de los caracteres

--- Proyecto Analizador Lexico.cpp
+++ Proyecto Analizador Lexico.cpp
@@ -3,24 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void PalabrasReservadas();//Esta funcion se encarga de buscar entre el arreglo de palabras reservadas
+void AnalizadorDeCaracter(char letra); //Funcion que leera los caracteres
+void Reservadas();//Esta funcion se encarga de buscar entre el arreglo de palabras reservadas
 
-const char *Palabrasreservadas[] = {"inicio","definir","repetir", "decimal","true","false""fin","mientras","si","sino","encender","apagar","string", "int", "boolean"}; //palabras reservadas
+const char *reservadas[] = {"inicio","definir","repetir", "decimal","true","false""fin","mientras","si","sino","encender","apagar","string", "int", "boolean"}; //palabras reservadas
 
-int IndicadorTam=sizeof(Palabrasreservadas)/sizeof(char *);//tamanio de las reservadas y el tamanio de memoria de el char
+int IndicadorTam=sizeof(reservadas)/sizeof(char *);//tamanio de las reservadas y el tamanio de memoria de el char
 int i; //Auxiliar de los ciclos
-char palabraIngresada[50]; //Se encarga de guardar y almacenar caracteres
+char palabraIngresada[50]; //Se encarga de guardar y almacenar los caracteres concatenados 
 char temp[2]; //temporal para guardar caracteres de una cadena
-//Estos son los contadores de los elementos de la tabla
-int contVariables=0;
-int contrNumeros=0;
-int contPalabrasReservadas=0;
-int contSimbolos=0;
-int contSignos=0;
-int contApagado=0;
-int contCiclos=0;
-int contCondicional=0;
-int contEncender=0;
 
 
 int main(){
@@ -28,37 +19,35 @@ int main(){
 	printf("---- ANALIZADOR LEXICO en C ---\n");
 	
 	//Funcion para buscar en el arreglo de palabras reservadas
-	//strcmp se utiliza para comparar los caracteres de dos Strings
+	//strcmp se encarga de comparar caracter por caracter dos Strings
 	//reservadas[i] son las palabras reservadas que declaramos como variable global al inicio del programa
 	//identificados es la palabra que el analizador lexico encontro
-}
-void PalabrasReservadas(){
-	
+void Reservadas(){
 	for(int i=0;i<IndicadorTam;i++){
-		if(strcmp(Palabrasreservadas[i],palabraIngresada)==0){
+		if(strcmp(reservadas[i],palabraIngresada)==0){
 		
 			//Contador Encender
-			if(strcmp(Palabrasreservadas[0],palabraIngresada)==0)
-				contEncender++;
+			if(strcmp(reservadas[0],palabraIngresada)==0)
+				contadorEncender++;
 			
 			//Contador Apagar
-			if(strcmp(Palabrasreservadas[1],palabraIngresada)==0)
-				contApagado++;
+			if(strcmp(reservadas[1],palabraIngresada)==0)
+				contadorApagado++;
 			
 			//Contadorr Ciclo Repetir
-			if(strcmp(Palabrasreservadas[4],palabraIngresada)==0)
-				contCiclos++;
+			if(strcmp(reservadas[4],palabraIngresada)==0)
+				contadorCiclos++;
 				
 			//Contador Ciclo Mientras
-			if(strcmp(Palabrasreservadas[6],palabraIngresada)==0)
-				contCiclos++;
+			if(strcmp(reservadas[6],palabraIngresada)==0)
+				contadorCiclos++;
 			
 			//Contador Condicional
-			if(strcmp(Palabrasreservadas[7],palabraIngresada)==0)
-				contCondicional++;
+			if(strcmp(reservadas[7],palabraIngresada)==0)
+				contadorCondicional++;
 				
 			//Cuenta las palabras reservadas que encuentre
-			contPalabrasReservadas++;
+			contadorPalabrasReservadas++;
 			palabraIngresada[0]='\0';
 			break;
 		}
@@ -66,5 +55,145 @@ void PalabrasReservadas(){
 			exit(-1);
 		}
 	}		
+}
+
+void AnalizadorDeCaracter(char letra){
+	if(letra==32||letra=='}'||letra=='{'
+		||letra=='('||letra==')'||letra==';'){
+			Estados();
+		}
+		
+		if( letra>='A' && letra<='Z' ){
+			if(Estado==q0){
+				Estado=q1;
+			}else if(Estado==q1||Estado==q2){
+				Estado=q2;
+			}
+			else if(Estado!=q0 && Estado!=q1 && Estado!=q2){
+				exit(-1);
+			}
+		}
+		
+		if(letra>='a' && letra<='z'){
+			if(Estado==q0){
+				temp[0]=letra; 
+				strcat(palabraIngresada,temp); 
+				Estado=q3;
+			}else if(Estado==q1||Estado==q2){
+				Estado=q2;
+			}
+			else if(Estado==q3){
+				temp[0]=letra; 
+				strcat(palabraIngresada,temp);
+				Estado=q3;}
+			else{
+				exit(-1);
+			}
+		}
+		if(letra<='9'&&letra>='0'){
+			if(Estado==q0){
+				Estado=q4;
+			}else if(Estado==q4||Estado==q5){
+				Estado=q5;
+			}else if(Estado==q13||Estado==q6){
+				Estado=q6;
+			}else if(Estado==q1||Estado==q2){
+				Estado=q2;
+			}else if(Estado==q7){
+				Estado=q4;
+			}else{
+				exit(-1);
+			}
+		}
+		if(letra=='+'){
+			if(Estado==q0){
+				Estado=q8;
+			}else if(Estado==q8){
+				Estado=q17;
+			}
+		}
+		
+		if(letra=='.'){
+			if((letra)<'1' || (letra)>='9'){
+				Estado=q19;
+			}else{
+				if(Estado==q4||Estado==q5&&((letra)=='1'||(letra)=='2'
+				||(letra)=='3'||(letra)=='4'||(letra)=='5'||(letra)=='6'
+				||(letra)=='7'||(letra)=='8'||(letra)=='9')){
+					Estado=q20;
+				}
+				else if(Estado!=q4 && Estado!=q5){
+					exit(-1);
+				}
+			}
+		}
+		if(letra=='^'){
+			if(Estado==q0){
+				Estado=q18;
+			}
+			else{
+				exit(-1);
+			} 
+		}
+		
+		if(letra=='-'){
+			if(Estado==q0){
+				Estado=q7;
+			}else if(Estado==q12){
+				Estado=q10;}
+		}	else if(Estado==q7){
+			Estado=q17;
+		}
+		
+		if(letra=='='){
+			if(Estado==q0){
+				Estado=q9;
+			}else if(Estado==q18){
+				Estado=q11;
+			}
+		}else if(Estado==q12){
+			Estado=q16;
+		}else if(Estado==q13){
+			Estado=q15;
+		}
+		
+		
+		if(letra=='/'||letra=='*'){
+			if(Estado==q0){
+				Estado=q18;
+			}else{
+				exit(-1);
+			}
+		}
+		
+		if(letra=='<'){                
+			if(Estado==q0){
+				Estado=q12;
+			}else{
+				exit(-1);
+			}
+		}
+		if(letra=='>'){   
+			if(Estado==q0){
+				Estado=q14;
+			}else{
+				exit(-1);
+			}
+		}	
+		
+		
+		if(letra=='!'){
+			if(Estado==q0){
+				Estado=q18;
+			}else{
+				exit(-1);
+			} 
+		}
+
+} 
+
+	
+
+   
 }
 
